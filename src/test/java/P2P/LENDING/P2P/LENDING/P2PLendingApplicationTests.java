d654e57b14324c92020e4d0e523278ea59f5cd1e
KaranBharatpe <karan.dhiman@bharatpe.com>
2024-04-11 17:49:41 +0530

    fc

---
+++ src/test/java/P2P/LENDING/P2P/LENDING/P2PLendingApplicationTests.java
@@ -0,0 +1,13 @@
+package P2P.LENDING.P2P.LENDING;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2PLendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
